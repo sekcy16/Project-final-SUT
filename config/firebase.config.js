9908b706cff2350bd686f871c2139342443d2e67
SC16 <97398918+sekcy16@users.noreply.github.com>
2025-03-15 23:27:41 +0700

    Update firebase.config.js

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -4,13 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDh68doOeuGJAOXE0RzDCaiKO-DOpp52Hk",
-  authDomain: "finalprojectdb-1a1aa.firebaseapp.com",
-  projectId: "finalprojectdb-1a1aa",
-  storageBucket: "finalprojectdb-1a1aa.appspot.com",
-  messagingSenderId: "424749228418",
-  appId: "1:424749228418:web:01203dc0677dbc4330ef9b",
-  measurementId: "G-6M1K204BGW"
+
 };
 
 let app, firebaseAuth, firebaseDB;
